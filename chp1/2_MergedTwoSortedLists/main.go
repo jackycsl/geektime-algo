b93c70a81561f0c5ae21c660f74dde60a674a704
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2021-11-21 08:37:08 +0800

    chp1 - q1,2

---
+++ chp1/2_MergedTwoSortedLists/main.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	protect := &ListNode{0, nil}
+	prev := protect
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			prev.Next = l1
+			l1 = l1.Next
+		} else {
+			prev.Next = l2
+			l2 = l2.Next
+		}
+		prev = prev.Next
+	}
+	if l1 == nil {
+		prev.Next = l2
+	} else {
+		prev.Next = l1
+	}
+	return protect.Next
+}
